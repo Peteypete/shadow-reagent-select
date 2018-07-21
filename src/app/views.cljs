e71c6142b72bd0d4e6a6ab1b82d627d86a14b781
Pete <peter@peterwen.com>
2018-07-20 21:28:40 -0700

    initial commit

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,6 +1,11 @@
 (ns app.views
   (:require [app.state :refer [app-state]]
-            [app.events :refer [increment decrement]]))
+            [app.events :refer [increment decrement]]
+            [cljsjs.classnames]
+            [cljsjs.react-input-autosize]
+            [cljsjs.react-select]))
+
+(defonce value (r/atom nil))
 
 (defn header
   []
@@ -12,9 +17,23 @@
   [:div
    [:button.btn {:on-click #(decrement %)} "-"]
    [:button {:disabled true} (get @app-state :count)]
-   [:button.btn {:on-click #(increment %)} "+"]])
+   [:button.btn {:on-click #(increment %)} "+"]])(defonce value (r/atom nil))
+
+(defn select-ui []
+  [:> js/window.Select {:value @value
+                        :options #js [#js {:value "a" :label "alpha"}
+                                      #js {:value "b" :label "beta"}
+                                      #js {:value "c" :label "gamma"}]
+                        :onChange #(reset! value (aget % "value"))}])
+
+(defn root-ui []
+  [:div {:style {:width 400}}
+   [:h3 "Select test"]
+   [select-ui]])
+
 
 (defn app []
   [:div
    [header]
-   [counter]])
+   [counter]
+   [root-ui]])
